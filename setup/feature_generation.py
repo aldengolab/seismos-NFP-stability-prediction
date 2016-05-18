3a93f5fc24237a61edcb0b78a6e79b490ca83424
Alden Golab <agolab@Aldens-MacBook-Pro.local>
2016-05-18 07:50:26 -0500

    Heading to class

--- setup/feature_generation.py
+++ setup/feature_generation.py
@@ -7,7 +7,7 @@ from acg_process import *
 
 LABEL_THRESHOLD = .2
 
-def generate_features(year1, year2, year3=None, test_year=None):
+def generate_features(data, year1, year2, year3=None, test_year=None):
     '''
     Runs all feature generation code. For our purposes, we train the model
     using 2012 and 2013 data and test on 2014. In order to make the feature
@@ -18,17 +18,25 @@ def generate_features(year1, year2, year3=None, test_year=None):
     - Columns are named: "YYYY_variablename" where YYYY is the year and
       variablename is the column label from the IRS.
     '''
+    features = pd.DataFrame(index = data.index)
     pass
 
-def generate_label(year1, year2):
+def generate_label(data, features, year1, year2, test_year):
     '''
     Generates 0/1 variable for YOY Gross Revenue negative change from year1
     to year2 greater than LABEL_THRESHOLD.
     '''
-    year1 = str(year1)
-    year2 = str(year2)
+    base_year = str(year1)
+    second_year = str(year2)
 
-    base =
+    base = numpy.array(data[base_year + 'totrevenue'])
+    second = numpy.array(data[second_year + 'totrevenue'])
+    change = (second - base) / base
+    change_index = (change / np.average(change)) * 100
+
+    YOY_revenue_fell = change_index < -20
+    YOY_revenue_fell = pd.DataFrame(YOY_revenue_fell)
+    pd.concat([features, YOY_revenue_fell])
 
 def generate_missing_for_year():
     '''
