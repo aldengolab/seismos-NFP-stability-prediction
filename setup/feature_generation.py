5edef0baabb1f1ddd96a099cfa6544070df121eb
Alden Golab <agolab@wireless-s1-so-150-55-229.uchicago.edu>
2016-05-25 15:08:49 -0500

    Added missing year generation

--- setup/feature_generation.py
+++ setup/feature_generation.py
@@ -5,6 +5,7 @@ import acg_read
 import acg_process
 import numpy as np
 import pandas as pd
+import sys
 
 MIXED_COLS = [4,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111, 112,113,114,115,116,119,121,122,123,124,125,126,127,128,129,130,131,132,134,135,136,137,138,139,140,145,147,150,151]
 
@@ -40,24 +41,11 @@ def generate_features(data, year1, year2, year3=None):
     '''
     features = pd.DataFrame(index = data.index)
     features = generate_rev_fall(data, features, year1, year2)
+    features = generate_YOY_rev_change(data, features, year1, year2)
     if year3:
         features = generate_rev_fall(data, features, year2, year3)
+        features = generate_YOY_rev_change(data, features, year2, year3)
     return features
-
-def generate_rev_fall(data, features, year1, year2, threshold = -0.2):
-    '''
-    Generates 0/1 variable for YOY Gross Revenue negative change from year1
-    to year2. Uses threshold to determine what negative change to mark as 1 
-    (e.g. if threshold is -20, will give values less than -20 a 1).
-    '''
-    calc = generate_YOY_rev_change(data, features, year1, year2, False)
-    # Assign True to values that are below threshold
-    second_year = str(year2)
-    column_name = second_year + '_YOY_revenue_fell'
-    calc.dropna(inplace = True)
-    calc[column_name] = calc[second_year + '_rev_change'] < threshold
-    # Returns features dataframe with new column
-    return features.join(calc[column_name])
 
 def generate_YOY_rev_change(data, features, year1, year2, add_to_features=True):
     '''
@@ -84,12 +72,51 @@ def generate_YOY_rev_change(data, features, year1, year2, add_to_features=True):
     else: 
         return features.join(calc[second_year + '_rev_change'])
     
-def generate_missing_for_year():
+def generate_rev_fall(data, features, year1, year2, threshold = -0.2):
     '''
-    Generates a 0/1 variable for an EIN if missing from one year but present
-    in others.
+    Generates 0/1 variable for YOY Gross Revenue negative change from year1
+    to year2. Uses threshold to determine what negative change to mark as 1 
+    (e.g. if threshold is -20, will give values less than -20 a 1).
     '''
-    pass
-
+    calc = generate_YOY_rev_change(data, features, year1, year2, False)
+    # Assign True to values that are below threshold
+    second_year = str(year2)
+    column_name = second_year + '_YOY_revenue_fell'
+    calc.dropna(inplace = True)
+    calc[column_name] = calc[second_year + '_rev_change'] < threshold
+    # Returns features dataframe with new column
+    return features.join(calc[column_name])
+    
+def generate_missing_for_year(data, features):
+    '''
+    Generates a 0/1 variable for an EIN if tot_revenue is missing from one 
+    year but present in others.
+    '''
+    cols = []
+    for x in data.columns: 
+        if '_totrevenue' in x: 
+            cols.append(x)
+    for col in cols: 
+        features[col[:4] + '_missing'] = pd.notnull(data[col])
+    
+    return features
+    
+def run(filename, new_filename, year1, num_years):
+    '''
+    Runs all feature generation.
+    '''
+    data = read_file(filename, convert_types = True)
+    if num_years == 2: 
+        features = generate_features(data, year1, year1 + 1)
+    if num_years == 3:
+        features = generate_features(data, year1, year1 + 1, year1 + 2)
+    features.to_csv(new_filename)
+    print "Wrote file to {}".format(new_filename)
+            
 if __name__ == "__main__":
-    pass
+    if len(sys.argv) == 1:
+        print "This program produces all features for model generation."
+        print "It will write the file features data to a separate file.\n"
+        print "Usage: python feature_generation.py <DATA FILE> <WRITE FILE> <FIRST YEAR> <NUMBER OF YEARS>"
+    else: 
+        run(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
