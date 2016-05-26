acdcda81fa99775d1cbe35bb89471712a56dbe30
Alden Golab <agolab@Aldens-MacBook-Pro.local>
2016-05-25 23:45:25 -0500

    Last fix for the evening

--- setup/feature_generation.py
+++ setup/feature_generation.py
@@ -12,7 +12,7 @@ import math
 # convert_types in read_file below.
 MIXED_COLS = [4,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111, 112,113,114,115,116,119,121,122,123,124,125,126,127,128,129,130,131,132,134,135,136,137,138,139,140,145,147,150,151]
 
-def read_file(filename, convert_types = False, drop_duplicates = False):
+def read_file(filename, convert_types = False, drop_duplicates = True):
     '''
     Reads csv file.
     '''
