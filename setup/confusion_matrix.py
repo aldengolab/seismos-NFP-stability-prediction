2b5e6f640295480705ae74d3f880835343ca340e
Paul <mackp@uchicago.edu>
2016-06-04 16:09:13 -0500

    modified to work with 2015 data

--- setup/confusion_matrix.py
+++ setup/confusion_matrix.py
@@ -1,22 +1,24 @@
 import pandas as pd
+import sys
 
-df = pd.read_csv("../data/features_v1_5-25-16.csv")
+df = pd.read_csv(sys.argv[1])
 
-dfnew = df[df['2013_rev_change'].notnull() & df['2014_rev_change'].notnull()]
+original_length = len(df)
+dfnew = df[df['2013_YOY_revenue_fell'].notnull()]
+dfnew = dfnew[dfnew['2014_YOY_revenue_fell'].notnull()]
+print("dropped {} percent  due to one of the two labels  missing".format((original_length -  len(dfnew)) / original_length))
 
-drop2013 = dfnew['2013_rev_change'] <= -.2
-drop2014 = dfnew['2014_rev_change'] <= -.2
-notdrop2013 = dfnew['2013_rev_change'] > -.2
-notdrop2014= dfnew['2014_rev_change'] > -.2
+
 
 total = len(dfnew)
-TP = len(dfnew[drop2013 & drop2014])
-FP = len(dfnew[drop2013 & notdrop2014])
-FN = len(dfnew[notdrop2013 & drop2014])
-TN = len(dfnew[notdrop2013 & notdrop2014])
+TP = len(dfnew[dfnew['2013_YOY_revenue_fell'] & dfnew['2014_YOY_revenue_fell']])
+FP = len(dfnew[dfnew['2013_YOY_revenue_fell'] & (dfnew['2014_YOY_revenue_fell'] == False)])
+FN =  len(dfnew[(dfnew['2013_YOY_revenue_fell'] == False) & dfnew['2014_YOY_revenue_fell']])
+TN = len(dfnew[(dfnew['2013_YOY_revenue_fell'] == False) & (dfnew['2014_YOY_revenue_fell'] == False)])
 
 precision = TP / (TP + FP)
 recall = TP /  (TP + FN)
+accuracy = (TP + TN) / total
 print("Total  was",  total)
 print("True Positive Rate:", TP / total)
 print("False Positive Rate:", FP / total)
@@ -25,20 +27,26 @@ print("False Negative Rate:", FN / total)
 
 print("Precision is:",  precision)
 print("Recacll is:",  recall)
+print( "Accuracy is", accuracy)
 
 
 print("even more naive:")
 
 TP2 = 0
 FP2 = 0
-FN2 = len(dfnew[drop2014])
-TN2 = len(dfnew[notdrop2014])
+FN2 = len(dfnew[dfnew['2014_YOY_revenue_fell']])
+TN2 = len(dfnew[dfnew['2014_YOY_revenue_fell'] == False])
 
 print("precision: 0")
 print("recall: 0")
 print("accuracy:", TN2 / total)
 
 
-# print("Total Number of Drops Predicted for 2014 based on 2013"):
-# print(yhat_drop_2014)
-# print
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        assert '.csv' in sys.argv[1]
+        filename = int(sys.argv[1])
+    else:
+        print("This program analyzes a naive model on the dataset")
+        print("Usage: python confusion_matrix.py <DATA FILE>")
